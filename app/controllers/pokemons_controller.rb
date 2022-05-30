bf806ee489d824b81bf800307917e2e8f2474185
sh200000 <ts46144bam.sh.k@outlook.jp>
2022-05-30 23:01:12 +0900

    特殊物理切り替え・ランク切り替え

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -7,7 +7,7 @@ class PokemonsController < ApplicationController
   end
 
   def attack_pokemon
-    params_attack = "ドラパルト"
+    
     params_attack = params[:attack_pokemon_name]
     @attack_pokemon = Pokemon.like(name: "#{params_attack}")
     attack_pokemon = @attack_pokemon[0].attributes
@@ -16,18 +16,22 @@ class PokemonsController < ApplicationController
     attack_pokemon_characteristics = @attack_pokemon[0].characteristics
 
     technique_type = []
+    technique_kind = []
     techniques.each do |technique|
       technique_type << technique.type
+      technique_kind << technique.kind
     end
 
-    params_defense = "カビゴン"
+    
+
+    
     params_defense = params[:defense_pokemon_name]
     @defense_pokemon = Pokemon.like(name: "#{params_defense}")
     defense_pokemon = @defense_pokemon[0].attributes
     defense_pokemon_type = @defense_pokemon[0].types
     defense_pokemon_characteristics = @defense_pokemon[0].characteristics
     
-    render json:{ attack_pokemon: attack_pokemon, attack_pokemon_type:attack_pokemon_type, techniques:techniques, technique_type:technique_type, attack_pokemon_characteristics:attack_pokemon_characteristics, 
+    render json:{ attack_pokemon: attack_pokemon, attack_pokemon_type:attack_pokemon_type, techniques:techniques, technique_type:technique_type, technique_kind:technique_kind, attack_pokemon_characteristics:attack_pokemon_characteristics, 
                   defense_pokemon: defense_pokemon, defense_pokemon_type:defense_pokemon_type, defense_pokemon_characteristics:defense_pokemon_characteristics}
     #render json:{ attack_pokemon: attack_pokemon, attack_pokemon_type:attack_pokemon_type, techniques:techniques, technique_type: technique_type, attack_pokemon_characteristics: attack_pokemon_characteristics}
   end
--- app/javascript/calculation.js
+++ app/javascript/calculation.js
@@ -43,6 +43,7 @@ function calculation (){
   console.log("あ")
   const attackPokemonName  = document.getElementById("attack_pokemon_name");
   const attackLevel  = document.getElementById("attack_level");
+  const attackLetter  = document.getElementById("attack_letter");
   const attackStatus  = document.getElementById("attack_status");
   const attackIndividual  = document.getElementById("attack_individual");
   const attackEffort  = document.getElementById("attack_effort");
@@ -53,9 +54,20 @@ function calculation (){
     }
   })
   const attackRankForm  = document.getElementById("attack_rank_form");
+  const attackRankFormLabel  = document.getElementById("attack_rank_form_label");
+  const attackRankRise  = document.getElementById("attack_rank_rise");
+  const attackRankDown  = document.getElementById("attack_rank_down");
   const attackCharacteristic  = document.getElementById("attack_characteristic");
   const attackTool  = document.getElementById("attack_tool");
   const attackTechniqueName = document.getElementById('attack_technique_name');
+  const attackKindPhysics = document.getElementById("kind_physics");
+  const attackKindSpecial = document.getElementById("kind_special");
+  const kind  = document.querySelectorAll(".kind");
+  kind.forEach(function(list) {
+    if (list.checked){
+      kindValue = list.value;
+    }
+  })
   const attackPower  = document.getElementById("attack_power");
   const attackType  = document.getElementById("attack_type");
   const attackBurn  = document.getElementById("attack_burn");
@@ -65,6 +77,7 @@ function calculation (){
   const defensePhysicalStatus  = document.getElementById("defense_physical_status");
   const defensePhysicalIndividual  = document.getElementById("defense_physical_individual");
   const defensePhysicalEffort  = document.getElementById("defense_physical_effort");
+  const defenseLetter  = document.getElementById("defense_letter");
   const defenseDefenseStatus  = document.getElementById("defense_defense_status");
   const defenseDefenseIndividual  = document.getElementById("defense_defense_individual");
   const defenseDefenseEffort  = document.getElementById("defense_defense_effort");
@@ -75,6 +88,9 @@ function calculation (){
     }
   })
   const defenseRankForm  = document.getElementById("defense_rank_form");
+  const defenseRankFormLabel  = document.getElementById("defense_rank_form_label");
+  const defenseRankRise  = document.getElementById("defense_rank_rise");
+  const defenseRankDown  = document.getElementById("defense_rank_down");
   const defenseCharacteristic  = document.getElementById("defense_characteristic");
   const defenseTool  = document.getElementById("defense_tool");
   const defenseReflector  = document.getElementById("defense_reflector");
@@ -93,8 +109,10 @@ function calculation (){
     }
   })
   const gravity  = document.getElementById("gravity");
-  const damageFigure  = document.getElementById("damage_figure");
-
+  const damageFigureMax  = document.getElementById("damage_figure_max");
+  const damageFigureYellow  = document.getElementById("damage_figure_yellow");
+  const damageFigureRed  = document.getElementById("damage_figure_red");
+  const damageFigureMin  = document.getElementById("damage_figure_min");
 
 
   
@@ -110,9 +128,11 @@ function calculation (){
     console.log(attackEffort.value)
     console.log(attackPersonalityValue)
     console.log(attackRankForm.value)
+
     console.log(attackCharacteristic.value)
     console.log(attackTool.value)
     console.log(attackTechniqueName.value)
+    console.log(kindValue)
     console.log(attackPower.value)
     console.log(attackType.value)
     console.log(attackBurn.value)
@@ -150,6 +170,55 @@ function calculation (){
   });*/
 
 
+  attackRankRise.addEventListener("click", () => {
+    if(parseFloat(attackRankForm.value) < 6){
+      attackRankForm.value = parseFloat(attackRankForm.value) + 1
+      if(attackRankForm.value > 0){
+        attackRankFormLabel.innerHTML = `+${attackRankForm.value}`
+      }else if(attackRankForm.value == 0){
+        attackRankFormLabel.innerHTML = `±${attackRankForm.value}`
+      }else if(attackRankForm.value < 0){
+        attackRankFormLabel.innerHTML = `±${attackRankForm.value}`
+      }
+    }
+  })
+  attackRankDown.addEventListener("click", () => {
+    if(parseFloat(attackRankForm.value) > -6){
+      attackRankForm.value = parseFloat(attackRankForm.value) - 1
+      if(attackRankForm.value > 0){
+        attackRankFormLabel.innerHTML = `+${attackRankForm.value}`
+      }else if(attackRankForm.value == 0){
+        attackRankFormLabel.innerHTML = `±${attackRankForm.value}`
+      }else if(attackRankForm.value < 0){
+        attackRankFormLabel.innerHTML = `-${attackRankForm.value}`
+      }
+    }
+  })
+  defenseRankRise.addEventListener("click", () => {
+    if(parseFloat(defenseRankForm.value) < 6){
+      defenseRankForm.value = parseFloat(defenseRankForm.value) + 1
+      if(defenseRankForm.value > 0){
+        defenseRankFormLabel.innerHTML = `+${defenseRankForm.value}`
+      }else if(attackRankForm.value == 0){
+        defenseRankFormLabel.innerHTML = `±${defenseRankForm.value}`
+      }else if(defenseRankForm.value < 0){
+        defenseRankFormLabel.innerHTML = `±${defenseRankForm.value}`
+      }
+    }
+  })
+  defenseRankDown.addEventListener("click", () => {
+    if(parseFloat(defenseRankForm.value) > -6){
+      defenseRankForm.value = parseFloat(defenseRankForm.value) - 1
+      if(defenseRankForm.value > 0){
+        defenseRankFormLabel.innerHTML = `+${defenseRankForm.value}`
+      }else if(defenseRankForm.value == 0){
+        defenseRankFormLabel.innerHTML = `±${defenseRankForm.value}`
+      }else if(defenseRankForm.value < 0){
+        defenseRankFormLabel.innerHTML = `-${defenseRankForm.value}`
+      }
+    }
+  })
+
   attackBurn.addEventListener("click", () => {
     console.log("え")
     const form = document.getElementById("form");
@@ -178,8 +247,19 @@ function calculation (){
           attackPersonalityValue = list.value;
         }
       })
-      attackStatus.value = status_calculation(attack_pokemon.A,attackIndividual.value,attackEffort.value,attackLevel.value,attackPersonalityValue)
-
+      kind.forEach(function(list) {
+        if (list.checked){
+          kindValue = list.value;
+        }
+      })
+      if (kindValue == "ぶつり"){
+        attackLetter.innerHTML = "攻撃"
+        attackStatus.value = status_calculation(attack_pokemon.A,attackIndividual.value,attackEffort.value,attackLevel.value,attackPersonalityValue)
+      }
+      else if(kindValue == "とくしゅ"){
+        attackLetter.innerHTML = "特攻"
+        attackStatus.value = status_calculation(attack_pokemon.C,attackIndividual.value,attackEffort.value,attackLevel.value,attackPersonalityValue)
+      }
       while (attackCharacteristic.firstChild) {
         attackCharacteristic.removeChild(attackCharacteristic.firstChild);
       }
@@ -225,7 +305,14 @@ function calculation (){
           defensePersonalityValue = list.value;
         }
       })
+      if (kindValue == "ぶつり"){
+        defenseLetter.innerHTML = "防御"
       defenseDefenseStatus.value = status_calculation(defense_pokemon.B,defenseDefenseIndividual.value,defenseDefenseEffort.value,defenseLevel.value,defensePersonalityValue)
+      }else if(kindValue == "とくしゅ"){
+        defenseLetter.innerHTML = "特防"
+        defenseDefenseStatus.value = status_calculation(defense_pokemon.D,defenseDefenseIndividual.value,defenseDefenseEffort.value,defenseLevel.value,defensePersonalityValue)
+      }
+
       while (defenseCharacteristic.firstChild) {
         defenseCharacteristic.removeChild(defenseCharacteristic.firstChild);
       }
@@ -250,16 +337,76 @@ function calculation (){
       console.log(jsresponse.technique_type[0])
       attack_type = jsresponse.technique_type[attackTechniqueNameValueN].attributes.name
       attackType.value = attack_type
+
+      
+      attack_kind = jsresponse.technique_kind[attackTechniqueNameValueN].attributes.name
+      if(attack_kind == "ぶつり"){
+        attackKindPhysics.checked = true
+      }else if(attack_kind == "とくしゅ"){
+        attackKindSpecial.checked = true
+      }
+      kind.forEach(function(list) {
+        if (list.checked){
+          kindValue = list.value;
+        }
+      })
+      console.log(kindValue)
+
+
   });
   defenseHikari.addEventListener("click", () => {
     console.log("た")
     damage_number = damage_calculation(attackLevel.value,attackPower.value,attackStatus.value,defenseDefenseStatus.value)
     console.log(damage_number)
     console.log(damage_calculation(50,100,182,189))
-    damage_ratio = damage_number[0]/(defensePhysicalStatus.value)
-    damage_ratio_percent = damage_ratio * 100
-    console.log(damage_ratio_percent)
-    damageFigure.setAttribute("style", `width:${100 - damage_ratio_percent}%;`)
+    damage_ratio_max = damage_number[1]/(defensePhysicalStatus.value)
+    damage_ratio_min = damage_number[0]/(defensePhysicalStatus.value)
+    damage_ratio_max_percent = damage_ratio_max * 100
+    damage_ratio_min_percent = damage_ratio_min * 100
+    console.log(damage_ratio_max_percent)
+
+    if(damage_ratio_max_percent < 50 ){
+      console.log("も")
+      damageFigureYellow.removeAttribute("style", "display:block;")
+      damageFigureRed.removeAttribute("style", "display:block;")
+      damageFigureMax.setAttribute("style", `width:${100 - damage_ratio_max_percent}%;`)
+      damageFigureMin.setAttribute("style", `width:${damage_ratio_max_percent - damage_ratio_min_percent}%;`)
+    }else if(50 <= damage_ratio_max_percent && damage_ratio_max_percent < 75 && damage_ratio_min_percent < 50){
+      console.log("ま")
+      damageFigureRed.removeAttribute("style", "display:block;")
+      damageFigureMax.setAttribute("style", `width:${100 - damage_ratio_max_percent}%; background-color: orange;`)
+      damageFigureYellow.setAttribute("style", `width:${damage_ratio_max_percent - 50}%; display:block;` )
+      damageFigureMin.setAttribute("style", `width:${50 - damage_ratio_min_percent}%;`)
+    }else if(50 <= damage_ratio_max_percent && damage_ratio_max_percent < 75 && 50 <= damage_ratio_min_percent){
+      damageFigureYellow.removeAttribute("style", "display:block;")
+      damageFigureRed.removeAttribute("style", "display:block;")
+      damageFigureMax.setAttribute("style", `width:${100 - damage_ratio_max_percent}%; background-color: orange;`)
+      damageFigureMin.setAttribute("style", `width:${damage_ratio_max_percent - damage_ratio_min_percent}%; background-color: rgba(211, 167, 24, 0.966);`)
+    }else if(75 <= damage_ratio_max_percent && damage_ratio_max_percent <= 100 && damage_ratio_min_percent < 75){
+      damageFigureYellow.removeAttribute("style", "display:block;")
+      damageFigureMax.setAttribute("style", `width:${100 - damage_ratio_max_percent}%; background-color: red;`)
+      damageFigureRed.setAttribute("style", `width:${damage_ratio_max_percent - 75}%; display:block;` )
+      damageFigureMin.setAttribute("style", `width:${75 - damage_ratio_min_percent}%; background-color: rgba(211, 167, 24, 0.966);`)
+
+    }else if(75 <= damage_ratio_max_percent && damage_ratio_max_percent <= 100 && 75 <= damage_ratio_min_percent){
+      damageFigureYellow.removeAttribute("style", "display:block;")
+      damageFigureRed.removeAttribute("style", "display:block;")
+      damageFigureMax.setAttribute("style", `width:${100 - damage_ratio_max_percent}%; background-color: red;`)
+      damageFigureMin.setAttribute("style", `width:${damage_ratio_max_percent - damage_ratio_min_percent}%; background-color: rgba(160, 10, 10, 0.966);`)
+    
+    }else if(100 <= damage_ratio_max_percent && damage_ratio_min_percent <= 100){
+      damageFigureYellow.removeAttribute("style", "display:block;")
+      damageFigureRed.removeAttribute("style", "display:block;")
+      damageFigureMax.setAttribute("style", "width:0%;")
+      damageFigureMin.setAttribute("style", `width:${100 - damage_ratio_min_percent}%; background-color: rgba(160, 10, 10, 0.966);`)
+    }else{
+      damageFigureYellow.removeAttribute("style", "display:block;")
+      damageFigureRed.removeAttribute("style", "display:block;")
+      damageFigureMax.setAttribute("style", "width:0%;")
+      damageFigureMin.setAttribute("style", "width:0%")
+    }
+
+
 
   });
 };
